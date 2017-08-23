03b0a2a54a91a1985dffb8a3e328674349f9bf5f
Thanh Nga <thanhngabkav01@gmail.com>
2017-08-23 08:14:56 +0700

    Add Entities Class

---
+++ Source/VideoRental/BusinessLogic/Entities/DiskTitle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DAL.Entities
+{
+    public class DiskTitle
+    {
+        public DiskTitle()
+        {
+            this.Disks = new List<Disk>();
+            this.Reservations = new List<Reservation>();
+        }
+        [Key]
+        public int TitleID { set; get; }
+        
+        [Required]
+        public String Title { set; get; }
+
+        public String Tags { set; get; }
+
+        public byte[] Image { set; get; }
+
+        public int Quantity { set; get; }
+
+        [Required]
+        public int RentalRateID { set; get; }
+
+        [ForeignKey("RentalRateID")]
+        public virtual RentalRate RentalRate { set; get; }
+
+        public virtual ICollection<Disk> Disks { set; get; }
+
+        public virtual ICollection<Reservation> Reservations { set; get; }
+
+    }
+}
---
+++ Source/VideoRental/BusinessLogic/Entities/TransactionHistory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DAL.Entities;
+
+namespace DAL.Entities
+{
+    public class TransactionHistory
+    {
+        public TransactionHistory()
+        {
+            this.TransactionHistoryDetails = new List<TransactionHistoryDetail>();
+        }
+
+        [Key]
+        public int TransactionHistoryID { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { set; get; }
+        [Required]
+        public float TotalPurchaseCost { get; set; }
+
+        [Required]
+        [MaxLength(50,ErrorMessage = "Tên Trạng Thái Không Quá 50 Ký Tự")]
+        public String  Status { get; set; }
+
+        public String Note { get; set; }
+
+        public int ClerkID { set; get; }
+
+        public int CustomerID { set; get; }
+
+        [ForeignKey("ClerkID")]
+        public virtual User Clerk { set; get; }
+
+        [ForeignKey("CustomerID")]
+        public virtual Customer Customer { get; set; }
+
+        public virtual ICollection<TransactionHistoryDetail> TransactionHistoryDetails { set; get; }
+
+    }
+}
