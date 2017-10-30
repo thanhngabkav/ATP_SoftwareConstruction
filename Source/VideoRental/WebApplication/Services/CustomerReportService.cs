b85078047b8ec4fe06fad06db0d34785896d1290
Phi Nguyen <minhphi10896@outlook.com>
2017-10-30 16:05:47 +0700

    Add Customer rp class

---
+++ Source/VideoRental/WebApplication/Services/CustomerReportService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess.DAO;
+using DataAccess.Entities;
+namespace WebApplication.Services
+{
+    /// <summary>
+    /// Customer report service
+    /// </summary>
+    public class CustomerReportService : ICustomerReportService
+    {
+        public List<Customer> Report_AllCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> Report_LateFeeCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> Report_OverDueCustomer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/VideoRental/WebApplication/Services/ICustomerReportService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccess.DAO;
+using DataAccess.Entities;
+namespace WebApplication.Services
+{
+    public interface ICustomerReportService
+    {
+        /// <summary>
+        /// All customer report
+        /// </summary>
+        /// <returns></returns>
+        List<Customer> Report_AllCustomer();
+        /// <summary>
+        /// All customer have one or more  overdue item
+        /// </summary>
+        /// <returns></returns>
+        List<Customer> Report_OverDueCustomer();
+        /// <summary>
+        /// All customer have one or more late fee
+        /// </summary>
+        /// <returns></returns>
+        List<Customer> Report_LateFeeCustomer();
+    }
+}
