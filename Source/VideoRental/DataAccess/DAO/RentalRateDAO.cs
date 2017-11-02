8f9414e37f6a56c004fd35fac79544d373fdd92c
Nga Nguyen Thanh <thanhngabkav01@gmail.com>
2017-11-02 18:23:13 +0700

    Add RentalRate DAO API

--- Source/VideoRental/DataAccess/DAO/RentalRateDAO.cs
+++ Source/VideoRental/DataAccess/DAO/RentalRateDAO.cs
@@ -29,5 +29,24 @@ namespace DataAccess.DAO
             DateTime currentDate = DateTime.Today;
             return dbContext.RentalRates.Where(x => x.TitleID == diskTitleId).OrderByDescending(x => x.CreatedDate).FirstOrDefault();
         }
+
+        /// <summary>
+        /// Add new Rentalrate into database
+        /// </summary>
+        /// <param name="rentalRate"></param>
+        public void AddNewRentalRate(RentalRate rentalRate)
+        {
+            dbContext.RentalRates.Add(rentalRate);
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get All Rentalrates in database
+        /// </summary>
+        /// <returns></returns>
+        public List<RentalRate> GetAllRentalRates()
+        {
+            return dbContext.RentalRates.ToList();
+        }
     }
 }
