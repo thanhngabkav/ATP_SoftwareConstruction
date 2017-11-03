f996d505c2f85eeb49c687d08a47357b7849da72
Phi Nguyen <minhphi10896@outlook.com>
2017-11-03 10:33:26 +0700

    add show title infomation service

---
+++ Source/VideoRental/WebApplication/Services/TitleManagementService.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+using DataAccess.Entities;
+using DataAccess.Utilities;
+
+namespace WebApplication.Services
+{
+    public class TitleManagementService : ITitleManagementService
+    {
+        TitleDAO titleDAO;
+        public TitleInfoModel GetInfomationTitle(int titleID)
+        {
+            DiskTitle title = titleDAO.GetTitleById(titleID);
+            TitleInfoModel result = new TitleInfoModel();
+            result.TitleID = title.TitleID;
+            result.Title = title.Title;
+            result.Quantity = title.Quantity;
+            result.RentalPrice = title.RentalPrice;
+            result.LateChargePerDate = title.LateChargePerDate;
+            int rentable = 0;
+            foreach (Disk disk in title.Disks)
+            {
+                if (disk.Status.Equals(DiskStatus.RENTABLE)) rentable++;
+            }
+            result.NumberOfDiskRentable = rentable;
+            return result;
+        }
+    }
+}
