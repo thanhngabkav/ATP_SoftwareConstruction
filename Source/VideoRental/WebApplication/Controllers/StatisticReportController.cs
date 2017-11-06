8d9a61c70a346ea5b757b9ae8ca474c449dba578
Phi Nguyen <minhphi10896@outlook.com>
2017-11-06 22:19:35 +0700

    Add controller and view

---
+++ Source/VideoRental/WebApplication/Controllers/StatisticReportController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Services;
+using WebApplication.Models;
+namespace WebApplication.Controllers
+{
+    public class StatisticReportController : Controller
+    {
+        IStatisticReportService statisticReportService;
+        // GET: StatisticReport
+        public ActionResult Report_Title()
+        {
+            List<TitleReportModel> listResult = statisticReportService.Report_Title();
+            return View(listResult);
+        }
+    }
+}
