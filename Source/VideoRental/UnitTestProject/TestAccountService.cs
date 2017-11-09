016fc6fa104b359ac9fc80cc1f2e2f0ad06fdec6
Nga Nguyen Thanh <thanhngabkav01@gmail.com>
2017-11-09 19:07:48 +0700

    Refactor Test Account Service

--- Source/VideoRental/UnitTestProject/TestAccountService.cs
+++ Source/VideoRental/UnitTestProject/TestAccountService.cs
@@ -21,20 +21,38 @@ namespace UnitTestProject
         [TestMethod]
         public void TestLoginFail()
         {
-            AccountService accountService = new AccountService();
-            Mock<UserDAO> mockUserDAO = new Mock<UserDAO>();
             LoginModel loginModel = new LoginModel { Username = "wrong user name", Password = "fake" };
-            User fakeUser = new User();
+            User fakeUser = new User { Password = "user password" };
+
             //Mock data access layer
-            mockUserDAO.Setup(x => x.getUserByUserName(It.IsAny<String>())).Returns(fakeUser);
-            UserDAO userDAO = new UserDAO();
-            User user = userDAO.getUserByUserName(loginModel.Username);
-            Assert.IsNotNull(user);
+            var mockUserDAO = new Mock<UserDAO>();
+            AccountService accountService = new AccountService(mockUserDAO.Object);
+            mockUserDAO.Setup(x => x.getUserByUserName(loginModel.Username)).Returns(fakeUser);
+
             bool expectedResult = false;
             bool actualResult = accountService.Login(loginModel);
             Assert.AreEqual(expectedResult, actualResult);
-            mockUserDAO.Verify(x => x.getUserByUserName(It.IsAny<String>()), Times.AtLeastOnce);
+            mockUserDAO.Verify(x => x.getUserByUserName(loginModel.Username), Times.AtLeastOnce);
             
+        }
+
+        [TestMethod]
+        public void TestLoginPass()
+        {
+            LoginModel loginModel = new LoginModel { Username = "wrong user name", Password = "User pass word" };
+            SHA2Service sha2 = new SHA2Service();
+            String encodePassword = sha2.Encode("User pass word");
+            User fakeUser = new User { Password = encodePassword };
+
+            //Mock data access layer
+            var mockUserDAO = new Mock<UserDAO>();
+            AccountService accountService = new AccountService(mockUserDAO.Object);
+            mockUserDAO.Setup(x => x.getUserByUserName(loginModel.Username)).Returns(fakeUser);
+
+            bool expectedResult = true;
+            bool actualResult = accountService.Login(loginModel);
+            Assert.AreEqual(expectedResult, actualResult);
+            mockUserDAO.Verify(x => x.getUserByUserName(loginModel.Username), Times.AtLeastOnce);
         }
 
     }
--- Source/VideoRental/WebApplication/Services/AccountService.cs
+++ Source/VideoRental/WebApplication/Services/AccountService.cs
@@ -13,19 +13,32 @@ namespace WebApplication.Services
 {
     public class AccountService : IAccountService
     {
+        private UserDAO userDAO;
+        private SHA2Service sha2;
+        public AccountService(UserDAO userDAO)
+        {
+            this.userDAO = userDAO;
+            this.sha2 = new SHA2Service();
+        }
+
+        public AccountService()
+        {
+            this.userDAO = new UserDAO();
+            this.sha2 = new SHA2Service();
+        }
+
         /**
          * Check login info
          * @param loginModel : LoginModel
          * @return bool : true if login info is correct and false if not
          * */
-        public virtual bool Login(LoginModel loginModel)
-        {
-            UserDAO userDAO = new UserDAO();
-            SHA2Service sha2 = new SHA2Service();
+        public bool Login(LoginModel loginModel)
+        { 
+            
             User user = userDAO.getUserByUserName(loginModel.Username);
             if (user != null)
             {
-                TagDebug.D(GetType(), sha2.Encode(loginModel.Password)+"");
+               // TagDebug.D(GetType(), sha2.Encode(loginModel.Password)+"");
                 return user.Password.Equals(sha2.Encode(loginModel.Password));
             }
             return false;
